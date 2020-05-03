9a79a1b63b8c5c25c64a9616fa1068487fb0f9ad
Yunus Niandy S <yunusw718@gmail.com>
2020-05-03 22:18:26 +0700

    change download message

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -94,10 +94,10 @@ def get_readable_message():
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
                        f"{download.size()}" \
-                       f" at {download.speed()}, ETA: {download.eta()} "
+                       f"\nDL: {download.speed()}, ETA: {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"| P: {download.aria_download().connections} " \
+                    msg += f"\n P: {download.aria_download().connections} " \
                            f"| S: {download.aria_download().num_seeders}"
                 msg += f"\nGID: <code>{download.gid()}</code>"
             msg += "\n\n"
